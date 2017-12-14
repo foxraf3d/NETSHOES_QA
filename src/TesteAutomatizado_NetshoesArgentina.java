d7641c381709aa53e15e607aba85bcb35e13dd00
rafael simplicio <foxraf3d@gmail.com>
2017-12-14 18:50:25 -0200

    ZATTINI e NetShoes Argentina

---
+++ src/TesteAutomatizado_NetshoesArgentina.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteAutomatizado_NetshoesArgentina {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
