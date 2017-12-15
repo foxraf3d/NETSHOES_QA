60f022fb9775a17f87e0717fcb297043b001b98c
rafael simplicio <foxraf3d@gmail.com>
2017-12-15 16:50:21 -0200

    Atualizacao Selenium

---
+++ src/BaseTest.java
@@ -0,0 +1,91 @@
+import java.util.Map;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+
+
+public class BaseTest {
+
+	FirefoxDriver fDriver;
+	ChromeDriver cDriver;
+	InternetExplorerDriver ieDriver;
+
+	public BaseTest(){
+
+		/*FirefoxOptions optionFirefox = new FirefoxOptions();
+		optionFirefox.addArguments("test-type");
+		optionFirefox.addArguments("start-maximized");
+		optionFirefox.addArguments("--js-flags=--expose-gc");
+		optionFirefox.addArguments("--enable-precise-memory-info");
+		optionFirefox.addArguments("--disable-popup-blocking");
+		optionFirefox.addArguments("--disable-default-apps");
+		optionFirefox.addArguments("test-type=browser");
+		optionFirefox.addArguments("disable-infobars");
+
+		ChromeOptions optionChrome = new ChromeOptions();
+		optionChrome.addArguments("test-type");
+		optionChrome.addArguments("start-maximized");
+		optionChrome.addArguments("--js-flags=--expose-gc");
+		optionChrome.addArguments("--enable-precise-memory-info");
+		optionChrome.addArguments("--disable-popup-blocking");
+		optionChrome.addArguments("--disable-default-apps");
+		optionChrome.addArguments("test-type=browser");
+		optionChrome.addArguments("disable-infobars");
+
+*/
+		InternetExplorerOptions optionIE = new InternetExplorerOptions(); 
+		optionIE.addCommandSwitches("test-type");
+		optionIE.addCommandSwitches("start-maximized");
+		optionIE.addCommandSwitches("--js-flags=--expose-gc");
+		optionIE.addCommandSwitches("--enable-precise-memory-info");
+		optionIE.addCommandSwitches("--disable-popup-blocking");
+		optionIE.addCommandSwitches("--disable-default-apps");
+		optionIE.addCommandSwitches("test-type=browser");
+		optionIE.addCommandSwitches("disable-infobars");
+
+
+
+		fDriver = new FirefoxDriver(/*optionFirefox*/);
+		cDriver = new ChromeDriver(/*optionChrome*/);
+		ieDriver = new InternetExplorerDriver(optionIE);
+
+	}
+
+	//Abrindo o navegador
+	protected void AbrirNavegador(String navegador, String endereco){
+		switch (navegador){
+		case "Firefox":
+			fDriver.get(endereco);
+			break;
+
+		case "Chrome":
+			cDriver.get(endereco);
+			break;
+
+		case "IE":
+			ieDriver.get(endereco);
+			break;
+		}
+	}
+
+	//Fechando o Navegador
+	protected void FecharNavegador(String navegador){
+		switch (navegador){
+		case "Firefox":
+			fDriver.quit();;
+			break;
+
+		case "Chrome":
+			cDriver.quit();
+			break;
+
+		case "IE":
+			ieDriver.quit();
+			break;
+		}
+	}
+}
--- src/TesteAutomatizado_ZATTINI.java
+++ src/TesteAutomatizado_ZATTINI.java
@@ -1,12 +1,31 @@
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Test;
 
-public class TesteAutomatizado_ZATTINI {
-
+public class TesteAutomatizado_ZATTINI extends BaseTest {
+	
+	String endereco = "https://www.zattini.com.br/";
+	String navegador = null;
+	String [] browser;
+	
+	public TesteAutomatizado_ZATTINI(){
+		browser = new String[3];
+		browser[0] = "Firefox";
+		browser[1] = "Chrome";
+		browser[2] = "IE";
+	}
+	
+	@After
+	public void FecharNavegador(){
+		FecharNavegador(navegador);		
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Zattini_IncluirProduto_Firefox() {
+	    
+		AbrirNavegador(browser[1], endereco);
+	    
 	}
 
 }
